425962c5b79996b98f31927241af3ad9bd8bb32d
Balthasar Hofer <lebalz@outlook.com>
2020-08-23 18:02:22 +0200

    add some variations of lego heads

--- lego_heads/head_a_no_loops.py
+++ lego_heads/head_a_no_loops.py
@@ -1,5 +1,8 @@
 from gbsl_turtle import *
 
+goto(0, 0, draw=False)
+setheading(0)
+
 forward(120)
 left()
 forward(120)
@@ -32,8 +35,6 @@ forward(20)
 left()
 
 goto(10, 50, draw=False)
-
-setheading(0)
 
 right()
 forward(10)
---
+++ lego_heads/head_b_for.py
@@ -0,0 +1,46 @@
+from gbsl_turtle import *
+
+goto(0, 0, draw=False)
+setheading(0)
+
+for counter in range(4):
+    forward(120)
+    left()
+
+goto(20, 80, draw=False)
+
+for counter in range(4):
+    forward(20)
+    left()
+
+
+goto(70, 90, draw=False)
+
+for counter in range(2):
+    forward(40)
+    left()
+    forward(10)
+    left()
+
+goto(20, 40, draw=False)
+
+setheading(0)
+
+for counter in range(3):
+    right()
+    forward(10)
+    left()
+    forward(10)
+
+forward(20)
+
+for counter in range(3):
+    forward(10)
+    left()
+    forward(10)
+    right()
+
+backward(80)
+
+hideturtle()
+Screen().exitonclick()
