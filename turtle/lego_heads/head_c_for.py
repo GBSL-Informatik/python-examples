9301cbbb1161abc4604503ae7980927f9016407a
Balthasar Hofer <lebalz@outlook.com>
2020-09-20 16:51:16 +0200

    add more lego heads

--- turtle/lego_heads/head_c_for.py
+++ turtle/lego_heads/head_c_for.py
@@ -1,42 +1,33 @@
 from gbsl_turtle import *
 
-goto(-40, 0, draw=False)
-Screen().tracer(2)
+goto(-30, 0, draw=False)
 
 for counter in range(8):
-    forward(80)
+    forward(60)
     left(45)
 
-goto(-30, 140, draw=False)
+goto(-30, 100, draw=False)
 setheading(180)     # Orientierung nach links: ◀️
 
 for counter in range(8):
-    forward(10)
+    forward(5)
     right(45)
 
 
-goto(30, 140, draw=False)
+goto(30, 100, draw=False)
 setheading(0)       # Orientierung nach rechts: ▶️
 
 
 for counter in range(8):
-    forward(10)
+    forward(5)
     left(45)
 
 
-goto(0, 80, draw=False)
-setheading(22.5)
-
-for counter in range(8):
-    forward(8)
-    left(45)
-
-goto(-72, 60, draw=False)
+goto(-48, 40, draw=False)
 setheading(-45)     # Orientierung nach rechts unten: ↘️
 
 for counter in range(3):
-    forward(60)
+    forward(40)
     left(45)
 
-hideturtle()
-Screen().exitonclick()
+done()
--- turtle/lego_heads/head_c_while.py
+++ turtle/lego_heads/head_c_while.py
@@ -1,52 +1,41 @@
 from gbsl_turtle import *
 
-goto(-40, 0, draw=False)
-Screen().tracer(2)
+goto(-30, 0, draw=False)
 
 counter = 0
 while counter < 8:
-    forward(80)
+    forward(60)
     left(45)
     counter = counter + 1
 
-goto(-30, 140, draw=False)
+goto(-30, 100, draw=False)
 setheading(180)     # Orientierung nach links: ◀️
 
 counter = 0
 while counter < 8:
-    forward(10)
+    forward(5)
     right(45)
     counter = counter + 1
 
 
-goto(30, 140, draw=False)
+goto(30, 100, draw=False)
 setheading(0)       # Orientierung nach rechts: ▶️
 
 
 counter = 0
 while counter < 8:
-    forward(10)
+    forward(5)
     left(45)
     counter = counter + 1
 
 
-goto(0, 80, draw=False)
-setheading(22.5)
-
-counter = 0
-while counter < 8:
-    forward(8)
-    left(45)
-    counter = counter + 1
-
-goto(-72, 60, draw=False)
+goto(-48, 40, draw=False)
 setheading(-45)     # Orientierung nach rechts unten: ↘️
 
 counter = 0
 while counter < 3:
-    forward(60)
+    forward(40)
     left(45)
     counter = counter + 1
 
-hideturtle()
-Screen().exitonclick()
+done()
